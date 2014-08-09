33160e3f890e3a6d1d2bd822cfe0826fc59969fe
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-08-09 21:03:21 +0300

    moved GUID type to wintypes

--- winapi/rpc.lua
+++ winapi/rpc.lua
@@ -4,18 +4,6 @@ setfenv(1, require'winapi')
 rpc = ffi.load'Rpcrt4'
 
 ffi.cdef[[
-typedef struct _GUID {
-	uint8_t data[16];
-} GUID;
-typedef GUID IID;
-typedef IID *LPIID;
-typedef GUID CLSID;
-typedef CLSID *LPCLSID;
-typedef GUID FMTID;
-typedef FMTID *LPFMTID;
-typedef IID* REFIID;
-
-typedef GUID UUID;
 typedef long RPC_STATUS;
 typedef unsigned short* RPC_WSTR;
 
--- winapi/wintypes.lua
+++ winapi/wintypes.lua
@@ -157,4 +157,23 @@ typedef HANDLE HGLOBAL;
 typedef HANDLE HLOCAL;
 typedef HANDLE GLOBALHANDLE;
 typedef HANDLE LOCALHANDLE;
+
+// from Guiddef.h
+
+typedef struct _GUID {
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+typedef GUID IID;
+typedef IID *LPIID;
+typedef GUID CLSID;
+typedef CLSID *LPCLSID;
+typedef GUID FMTID;
+typedef FMTID *LPFMTID;
+typedef IID* REFIID;
+
+typedef GUID UUID;
 ]]
