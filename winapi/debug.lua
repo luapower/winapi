092132c56fa4f78dd3cbc933a3c712e135a02b2d
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-08-09 21:02:42 +0300

    made debug module work with strict.lua

--- winapi/debug.lua
+++ winapi/debug.lua
@@ -3,19 +3,23 @@ setfenv(1, require'winapi.namespace')
 
 --disable stdout buffering so we can print-debug stuff
 io.stdout:setvbuf'no'
+io.stderr:setvbuf'no'
 
 --set strict mode for the whole winapi namespace
 local _G = _G
 local declared = {}
 
+local getinfo = debug.getinfo
+local rawget, rawset, _print = rawget, rawset, print
+
 function _M:__index(k)
 	if declared[k] then return nil end
-	if _G[k] ~= nil then return _G[k] end
+	if rawget(_G, k) ~= nil then return rawget(_G, k) end
 	error(string.format('Undefined winapi global %s', k), 2)
 end
 
 function _M:__newindex(k,v)
-	local w = debug.getinfo(2, 'S').what
+	local w = getinfo(2, 'S').what
 	if w == 'main' or w == 'C' or declared[k] then
 		declared[k] = true
 		rawset(self, k, v)
@@ -33,7 +37,6 @@ function findname(prefix, value)
 end
 
 --print that can be used in expressions and recurses into tables
-_print = print
 local function __print(indent,...)
 	if indent ~= '' then _print(indent,...) else _print(...) end
 	for i=1,select('#',...) do
